6652107e09f9c6b80e5f319cb971fdfbc1372bb8
Gustavo Iavorski <gustavo2dev@gmail.com>
2026-03-24 15:37:03 -0300

    erro

---
+++ ATIVIDADES_JAVA/src/ATIVIDADES2/001/Retangulo.java
@@ -0,0 +1,13 @@
+public class Retangulo {
+    
+    double largura;
+    double altura;
+
+    double calcularArea() {
+        return largura * altura;
+    }
+
+    double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
---
+++ ATIVIDADES_JAVA/src/ATIVIDADES2/001/ativ01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ativ01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Retangulo r = new Retangulo();
+
+        System.out.print("Digite a largura: ");
+        r.largura = sc.nextDouble();
+
+        System.out.print("Digite a altura: ");
+        r.altura = sc.nextDouble();
+
+        System.out.println("Área: " + r.calcularArea());
+        System.out.println("Perímetro: " + r.calcularPerimetro());
+
+        sc.close();
+    }
+}
